77e12a51ad5e7516cb0e11401dd787fb81b94ec4
rich98 <51236157+rich98@users.noreply.github.com>
2024-02-24 11:54:03 +0000

    Update 1-1000000000.py

--- 1-1000000000.py
+++ 1-1000000000.py
@@ -4,9 +4,12 @@ def sieve_of_eratosthenes(limit):
 
     for ind, val in enumerate(primes):
         if val is True:
-            print(ind)  # Print the prime number as it is found
             primes[ind*2::ind] = [False] * (((limit - 1)//ind) - 1)
 
-# Print prime numbers up to 1,000,000,000
+    return [ind for ind, val in enumerate(primes) if val is True]
+
+# Print prime numbers up to 1 billion 
 # WARNING: This will take a significant amount of time and memory.
-sieve_of_eratosthenes(1000000000)
+primes = sieve_of_eratosthenes(1000000000)
+for prime in primes:
+    print(prime)
